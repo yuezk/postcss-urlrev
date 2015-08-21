9445edbd24f03d401b99dea914ab6a4408508d3f
Kevin Yue <yuezk001@gmail.com>
2015-08-22 00:23:51 +0800

    it works well

--- index.js
+++ index.js
@@ -1,13 +1,146 @@
+'use strict';
+
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
+var url = require('url');
+var http = require('http');
+var https = require('https');
+var crypto = require('crypto');
+
 var postcss = require('postcss');
+
+function getUrlMeta(string) {
+    var reg = /url\((\s*)(['"]?)(.+?)\2(\s*)\)/;
+    var match = reg.exec(string);
+
+    return {
+        source: match[0],
+        before: match[1],
+        quote: match[2],
+        value: match[3],
+        after: match[4],
+        parsed: url.parse(match[3])
+    };
+}
+
+function isRemotePath(filePath) {
+    return /^https?:\/\//.test(filePath);
+}
+
+function getResourcePath(str, dirname) {
+    var filePath;
+
+    if (isRemotePath(str)) {
+        filePath = str;
+    } else {
+        filePath = path.resolve(dirname, url.parse(str).pathname);
+    }
+    return Promise.resolve(filePath);
+}
+
+function getRemoteFileHash(file) {
+    return new Promise(function (resolve, reject) {
+        var client = /^https/.test(file) ? https : http;
+        client.get(file, function (res) {
+            var md5 = crypto.createHash('md5');
+
+            res.on('data', function (chunk) {
+                md5.update(chunk);
+            });
+
+            res.on('end', function () {
+                resolve(md5.digest('hex'));
+            });
+        }).on('error', reject);
+    });
+}
+
+function getLocalFileHash(file) {
+    return fs.readFileAsync(file).then(function (contents) {
+        return crypto.createHash('md5').update(contents).digest('hex');
+    });
+}
+
+function getHash() {
+    return function (filepath) {
+        if (isRemotePath(filepath)) {
+            return getRemoteFileHash(filepath);
+        }
+        return getLocalFileHash(filepath);
+    };
+}
+
+function createUrl(urlMeta, opts) {
+    return function (hash) {
+        urlMeta.value = opts.replacer(urlMeta.value, hash);
+        return 'url(' +
+            urlMeta.before +
+            urlMeta.quote +
+            urlMeta.value +
+            urlMeta.quote +
+            urlMeta.after +
+        ')';
+    };
+}
+
+function defaultReplacer(hashLength) {
+    hashLength = Math.max(0, hashLength || 10);
+
+    return function (str, hash) {
+        var urlObj = url.parse(str, true);
+        var length = Math.min(hashLength, hash.length);
+
+        urlObj.query.v = hash.slice(0, length);
+        delete urlObj.search;
+
+        return url.format(urlObj);
+    };
+}
+
+
+function processDecl(result, decl, from, opts) {
+    var inputfile = decl.source && decl.source.input && decl.source.input.file;
+    var dirname = inputfile ? path.dirname(inputfile) : path.dirname(from);
+    var urlMeta = getUrlMeta(decl.value);
+
+    // ignore absolute urls, hashes or data uris
+    if (urlMeta.value.indexOf('/') === 0 ||
+        urlMeta.value.indexOf('data:') === 0 ||
+        urlMeta.value.indexOf('#') === 0
+    ) {
+        return Promise.resolve();
+    }
+
+    // do not handle the http/https urls if `includeRemote` is false
+    if (!opts.includeRemote && isRemotePath(urlMeta.value)) {
+        return Promise.resolve();
+    }
+
+    return getResourcePath(urlMeta.value, dirname)
+        .then(getHash())
+        .then(createUrl(urlMeta, opts))
+        .then(function (newUrl) {
+            decl.value = newUrl;
+        });
+}
 
 module.exports = postcss.plugin('postcss-urlrev', function (opts) {
     opts = opts || {};
+    opts.replacer = opts.replacer || defaultReplacer(opts.hashLength);
 
-    // Work with options here
+    return function (css, result) {
+        var from = result.opts.from
+            ? path.resolve(result.opts.from)
+            : '.';
 
-    return function (css) {
+        var actions = [];
+        css.eachDecl(function (decl) {
+            if (decl.value && decl.value.indexOf('url(') > -1) {
+                actions.push(processDecl(result, decl, from, opts));
+            }
+        });
 
-        // Transform CSS AST here
-
+        return Promise.all(actions);
     };
 });
--- test/test.js
+++ test/test.js
@@ -1,24 +1,141 @@
+var fs = require('fs');
+var path = require('path');
 var postcss = require('postcss');
 var expect  = require('chai').expect;
-
+var express = require('express');
 var plugin = require('../');
+require('mocha');
+
+var app = express();
+var server;
+app.use(express.static(path.join(__dirname, 'fixtures')));
+
+// md5 images/test.png => e19ac7dee652fdbcbaa7cf7f3b998fa1
+// md5 test-local.css => 85c1934d3ceb0ae4c4b7f6f3814a79c9
 
 var test = function (input, output, opts, done) {
-    postcss([ plugin(opts) ]).process(input).then(function (result) {
-        expect(result.css).to.eql(output);
-        expect(result.warnings()).to.be.empty;
-        done();
-    }).catch(function (error) {
-        done(error);
-    });
+    var css = fs.readFileSync(input, 'utf-8');
+    var expected = fs.readFileSync(output, 'utf-8');
+
+    postcss([plugin(opts)])
+        .process(css, { from: input })
+        .then(function (result) {
+            expect(result.css).to.eql(expected);
+            expect(result.warnings()).to.be.empty;
+            done();
+        }).catch(function (error) {
+            done(error);
+        });
 };
 
+before(function (done) {
+    server = app.listen(10010, function () {
+        var host = server.address().address;
+        var port = server.address().port;
+        console.log('Test server listening at http://%s:%s', host, port);
+
+        done();
+    });
+});
+
+after(function () {
+    server.close();
+});
+
 describe('postcss-urlrev', function () {
+    it(
+        'should add the local file\'s md5 hash to the end of the url',
+        function (done) {
+            var input = 'test/fixtures/test-local.css';
+            var output = 'test/expected/test-local.css';
+            test(input, output, { }, done);
+        }
+    );
 
-    /* Write tests here
+    it(
+        'should skip the data uri',
+        function (done) {
+            var input = 'test/fixtures/test-datauri.css';
+            var output = 'test/expected/test-datauri.css';
+            test(input, output, { }, done);
+        }
+    );
 
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
+    it(
+        'should skip the absolute path',
+        function (done) {
+            var input = 'test/fixtures/test-absolute.css';
+            var output = 'test/expected/test-absolute.css';
+            test(input, output, { }, done);
+        }
+    );
 
+    it(
+        'should append the hash to the end of the url if ' +
+        'other parameter already exists in the querystring',
+        function (done) {
+            var input = 'test/fixtures/test-append-hash.css';
+            var output = 'test/expected/test-append-hash.css';
+            test(input, output, { }, done);
+        }
+    );
+
+    it(
+        'should replace the value of `v`, if ' +
+        '`v` already exists in the query string',
+        function (done) {
+            var input = 'test/fixtures/test-replace-hash.css';
+            var output = 'test/expected/test-replace-hash.css';
+            test(input, output, { }, done);
+        }
+    );
+
+    it(
+        'should add custom hash when the `opts.replacer` is specified',
+        function (done) {
+            var input = 'test/fixtures/test-replacer.css';
+            var output = 'test/expected/test-replacer.css';
+            var replacer = function (url, hash) {
+                return url + '?' + hash;
+            };
+            test(input, output, { replacer: replacer }, done);
+        }
+    );
+
+    it(
+        'should handle the remote url if the `opts.includeRemote` is specified',
+        function (done) {
+            var input = 'test/fixtures/test-remote.css';
+            var output = 'test/expected/test-remote.css';
+            test(input, output, { includeRemote: true }, done);
+        }
+    );
+
+    it(
+        'should substitude the hash if the `opts.hashLength` is specified',
+        function (done) {
+            var input = 'test/fixtures/test-hash-length.css';
+            var output = 'test/expected/test-hash-length.css';
+            test(input, output, { hashLength: 22 }, done);
+        }
+    );
+
+    it(
+        'should append empty hash when the `opts.hashLength` is less than zero',
+        function (done) {
+            var input = 'test/fixtures/test-hash-length-zero.css';
+            var output = 'test/expected/test-hash-length-zero.css';
+            test(input, output, { hashLength: -1 }, done);
+        }
+    );
+
+    it(
+        'should append full hash if the `opts.hashLength` ' +
+        'is greater than the length of hash',
+        function (done) {
+            var input = 'test/fixtures/test-hash-length-max.css';
+            var output = 'test/expected/test-hash-length-max.css';
+            test(input, output, { hashLength: 100 }, done);
+        }
+    );
 });
